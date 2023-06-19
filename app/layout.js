df557c53a741ba1273e9431a04ffe1cce9a47d2c
Joshua <koffworld@gmail.com>
2023-06-19 18:58:42 +0000

    font changed to poppins

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,12 @@
 import './globals.css'
 import { Inter } from 'next/font/google'
+import { Poppins } from 'next/font/google'
 
 const inter = Inter({ subsets: ['latin'] })
+const poppins = Poppins({ 
+  weight: ["100","200","300","400","500","600","700","800","900"],
+  subsets: ['latin']
+})
 
 export const metadata = {
   title: 'Lyf | Home',
@@ -11,7 +16,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={poppins.className}>{children}</body>
     </html>
   )
 }
